20f9c1a487362cc2096f9195390ba9f8c104e642
Adarsh <thakuradarsh8101@gmail.com>
2022-06-23 20:48:45 +0530

    make it better

--- get.php
+++ get.php
@@ -1,40 +1,63 @@
 <?php
-  
-     
-   // connect the database with the server
-   $conn = new mysqli("208.91.198.197:3306","Phasor","admin@12345!","Phasor");
-     
-    // if error occurs 
-    if ($conn -> connect_errno)
-    {
-       echo "Failed to connect to MySQL: " . $conn -> connect_error;
-       exit();
-    }
-  
-    $course_data = "select * from courses";
-    $course_result = ($conn->query($course_data));
-    //declare array to store the data of database
-    $course = []; 
-  
-    if ($course_result->num_rows > 0) 
-    {
-        // fetch all data from db into array 
-        $course = $course_result->fetch_all(MYSQLI_ASSOC);  
-    }   
 
-    
-    $banner_data = "select * from images";
-    $banner_result = ($conn->query($banner_data));
-    //declare array to store the data of database
-    $banner = []; 
-  
-    if ($banner_result->num_rows > 0) 
-    {
-        // fetch all data from db into array 
-        $banner = $banner_result->fetch_all(MYSQLI_ASSOC);  
-    }   
+
+// connect the database with the server
+$conn = new mysqli("208.91.198.197:3306", "Phasor", "admin@12345!", "Phasor");
+
+// if error occurs 
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: " . $conn->connect_error;
+    exit();
+}
+
+$course_data = "select * from courses";
+$course_result = ($conn->query($course_data));
+//declare array to store the data of database
+$course = [];
+
+if ($course_result->num_rows > 0) {
+    // fetch all data from db into array 
+    $course = $course_result->fetch_all(MYSQLI_ASSOC);
+}
+
+
+$banner_data = "select * from banners";
+$banner_result = ($conn->query($banner_data));
+//declare array to store the data of database
+$banner = [];
+
+if ($banner_result->num_rows > 0) {
+    // fetch all data from db into array 
+    $banner = $banner_result->fetch_all(MYSQLI_ASSOC);
+}
+
+
+$faculty_data = "select * from faculty";
+$faculty_result = ($conn->query($faculty_data));
+//declare array to store the data of database
+$faculty = [];
+
+if ($faculty_result->num_rows > 0) {
+    // fetch all data from db into array 
+    $faculty = $faculty_result->fetch_all(MYSQLI_ASSOC);
+}
+
+
+$testimonial_data = "select * from testimonials";
+$testimonial_result = ($conn->query($testimonial_data));
+//declare array to store the data of database
+$testimonial = [];
+
+if ($testimonial_result->num_rows > 0) {
+    // fetch all data from db into array 
+    $testimonial = $testimonial_result->fetch_all(MYSQLI_ASSOC);
+}
 ?>
-<!--   -->
-<?php   
-    mysqli_close($conn);
+
+
+
+
+
+<?php
+mysqli_close($conn);
 ?>
